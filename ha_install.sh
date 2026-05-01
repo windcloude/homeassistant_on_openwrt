71fdd0e1df498c5832f811817ed8b92a7d8e94ec
windcloude <156591155+windcloude@users.noreply.github.com>
2026-05-01 22:22:24 +0800

    Update ha_install.sh

--- ha_install.sh
+++ ha_install.sh
@@ -221,13 +221,13 @@ $(version securetar)  # backup
 $(version pyudev)  # usb
 $(version pycognito)
 # $(version python-miio)  # xiaomi_miio
-$(version PyXiaomiGateway)
-$(version aiodhcpwatcher)  # dhcp
-$(version aiodiscover)  # dhcp
-$(version httpx)  # image/http
-$(version hassil)  # conversation
-$(version home-assistant-intents)  # conversation
-$(version paho-mqtt)  # mqtt
+# $(version PyXiaomiGateway)
+# $(version aiodhcpwatcher)  # dhcp
+# $(version aiodiscover)  # dhcp
+# $(version httpx)  # image/http
+# $(version hassil)  # conversation
+# $(version home-assistant-intents)  # conversation
+# $(version paho-mqtt)  # mqtt
 
 # fixed dependencies
 python-jose[cryptography]==3.2.0  # (pycognito dep) 3.3.0 is not compatible with the python3-cryptography in the feed
